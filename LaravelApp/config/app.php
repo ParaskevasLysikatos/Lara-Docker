e396d4ef717fb205798fe36b6173ab1055ff8e71
Paraskevas <vaslysalex@hotmail.gr>
2024-03-11 14:13:13 +0200

    drop redis

--- LaravelApp/config/app.php
+++ LaravelApp/config/app.php
@@ -183,7 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class
+       // 'Redis' => Illuminate\Support\Facades\Redis::class
     ])->toArray(),
 
 ];
